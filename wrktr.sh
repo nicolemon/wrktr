d104490d121a9b553589244833f4448506e0f554
Nicole Montano <n@nicolemon.com>
2025-11-06 00:05:59 -0800

    twek

--- wrktr.sh
+++ wrktr.sh
@@ -16,11 +16,6 @@ init() {
     git fetch
     git worktree add main
     git branch --list | grep -v '\*\|\+\|ahead' | xargs -n 1 git branch -D
-    mkdir -p .SHARED/.claude/commands
-    mkdir -p .SHARED/.claude/agents
-    touch .SHARED/.claude/settings.local.json
-    touch .SHARED/gitignore.Makefile
-    touch .SHARED/CLAUDE.local.md
 
     popd && echo "returned to parent directory"
 }
@@ -34,19 +29,6 @@ worktree_component() {
         printf "unknown"
     fi
 }
-
-# files under .SHARED you want hardlinked
-hardlink_assets=(
-    "gitignore.Makefile"
-    "CLAUDE.local.md"
-    ".claude/settings.local.json"
-)
-
-# directories under .SHARED you want softlinked
-softlink_assets=(
-    ".claude/commands"
-    ".claude/agents"
-)
 
 guard() {
     if [ ! $(worktree_component) = "project" ]; then
@@ -64,6 +46,11 @@ createHardlink() {
 createSoftlink() {
     filename="$1"
     ln -s ${shared}/${filename} ${worktree}/${filename}
+}
+
+createCopy() {
+    filename="$1"
+    cp -R ${shared}/${filename} ${worktree}/${filename}
 }
 
 _verifyExists() {
@@ -88,6 +75,13 @@ verifyHardlink() {
     [ "${link_count}" -gt 1 ] && (echo "  ✓ hardlinked: ${worktree}/${filename}") || (echo "  ✘ not hardlinked: ${worktree}/${filename}")
 }
 
+verifyCopy() {
+    filename="$1"
+    share_sum=$(sha1sum ${shared}/${filename} | awk '{ print $1 }')
+    work_sum=$(sha1sum ${worktree}/${filename} | awk '{ print $1 }')
+    [ "${share_sum}" = "${work_sum}" ] && (echo "  ✓ copy matches: ${worktree}/${filename}") || (echo "  ✘ copy mismatch: ${worktree}/${filename}")
+}
+
 check() {
     echo "\n🔎 Checking for shared local resources in 📁 ${worktree}...\n"
     _check
@@ -103,6 +97,10 @@ _check() {
         verifyExists ${asset} && verifySoftlink ${asset}
         echo
     done
+
+    for asset in ${copy_assets}; do
+        verifyExists ${asset} && verifyCopy ${asset}
+    done
 }
 
 link() {
@@ -114,6 +112,10 @@ link() {
 
     for asset in ${softlink_assets}; do
         _verifyExists ${asset} || createSoftlink ${asset}
+    done
+
+    for asset in ${copy_assets}; do
+        createCopy ${asset}
     done
 
     echo "\n🔗 Shared local resources linked in 📁 ${worktree}\n"
@@ -129,42 +131,85 @@ cleanup() {
         _verifyExists ${asset} && unlink ${worktree}/${asset}
     done
 
+    for asset in ${copy_assets}; do
+        _verifyExists ${asset} && cd ${worktree} && git restore ${asset}
+    done
+
     echo "\n✂️ Shared local resources unlinked in 📁 ${worktree}\n"
     _check
 }
 
 _exit() {
-    [ -n ${project_dir} ] && unset project_dir
-    [ -n ${shared} ] && unset shared
-    [ -n ${worktree} ] && unset worktree
-    [ -n ${hardlink_assets} ] && unset hardlink_assets
-    [ -n ${softlink_assets} ] && unset softlink_assets
+    [ -v project_dir ] && unset project_dir
+    [ -v shared ] && unset shared
+    [ -v worktree ] && unset worktree
+    [ -v hardlink_assets ] && unset hardlink_assets
+    [ -v softlink_assets ] && unset softlink_assets
+    [ -v copy_assets ] && unset copy_assets
     exit "$1"
 }
 
 entrypoint() {
+    if [ $1 = "help" ]; then
+        _help && _exit 0
+    fi
+
     if [ -z $1 ]; then
-        check . && exit 0
+        check . && _exit 0
     fi
 
     if [ $1 = "init" ]; then
         init $2 $3 && _exit 0
     fi
 
+    [ -f wrktr.conf ]
+
     if [ $(worktree_component) = "project" ] ; then
         project_dir=$(pwd)
         shared="${project_dir}/.SHARED"
         worktree="${project_dir}/$2"
-        guard
+        guard  # check for worktree directory
+
+        [ -f wrktr.conf ] && source wrktr.conf
+        [ ! -v hardlink_assets ] && echo "hardlink_assets is unset"
+        [ ! -v softlink_assets ] && echo "softlink_assets is unset"
+
         exec $1
     elif [ $(worktree_component) = "worktree" ]; then
         project_dir=".."
         shared="../${project_dir}/.SHARED"
         worktree=$(pwd)
+
+        [ -f ../wrktr.conf ] && source ../wrktr.conf
+        [ ! -v hardlink_assets ] && echo "hardlink_assets is unset"
+        [ ! -v softlink_assets ] && echo "softlink_assets is unset"
+
         exec $1
     else
-        echo "neither a worktree project nor worktree; run \`wrktr init [repo-url] [project-directory]\`" && exit _1
+        echo "neither a worktree project nor worktree; run \`wrktr init [repo-url] [project-directory]\`" && _exit 1
     fi
+}
+
+_help() {
+    cat <<EOF
+    wrktr init [repo-url] [project-directory]
+
+        1. Create project directory
+        2. Clone bare repository into .bare
+        3. Create .git in project directory
+        4. Configure fetch remote
+        5. Add main worktree
+        6. Delete local branches
+
+    wrktr link [worktree directory]
+    wrktr check [worktree directory]
+    wrktr cleanup [worktree directory]
+
+    wrktr.conf:
+        hardlink_assets=()
+        softlink_assets=()
+        copy_assets=()
+EOF
 }
 
 entrypoint "$@"
